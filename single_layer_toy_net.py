9baa1b72ece8c89ba2e1da13f3702e4975fb56a2
Dmitrijs Odinokijs <Dmitrijs.Odinokijs@edu.rtu.lv>
2020-02-04 13:45:15 +0200

    some more tweaks

--- single_layer_toy_net.py
+++ single_layer_toy_net.py
@@ -10,37 +10,21 @@ from tools import toolset
 import numpy as np
 
 
-dataExpected = toolset.dataGenByExpression('(x1**2) + (x1*x2**2)',0,1,10)
+dataExpected = toolset.dataGenByExpression('(x1**2) + (x1*x2**3) + 2*x1',0,1,33)
 
 # x1, x2 = symbols(('x1 x2'))
 
 # y = (x1**2) + (x1*x2**2)
 # plot3d(y,(x1,1,10),(x2,1,10))
 
-# X = np.array([
-#     [0, 0],
-#     [0, 1],
-#     [1, 0],
-#     [1, 1]
-# ])
 
-
-# Y = np.array([
-#     [0],
-#     [1],
-#     [1],
-#     [0]
-# ])
-
-
-
-X = dataExpected[:,0:2]
+X = dataExpected[:,0:np.shape(dataExpected)[1]-1]
 
 X_train = X
-Y = dataExpected[:,2]
+Y = dataExpected[:,np.shape(dataExpected)[1]-1]
 
 # add feature cross between 1st and 2nd feature
-## X_train = np.c_[X_train, X[:, 0]* X[:, 1]]  # "np.c_" concatenates data column-wise
+##X_train = np.c_[X_train, X[:, 0]* X[:, 1]]  # "np.c_" concatenates data column-wise
 
 # now we can set up data in the shape required by the neural net layers
 X_train = X_train.T
@@ -51,8 +35,8 @@ Y_train = Y.T
 #%%
 
 # define training constants
-learning_rate = 1
-number_of_epochs = 5000
+learning_rate = 0.001
+number_of_epochs = 1001
 
 np.random.seed(36) # set seed value so that the results are reproduceable
 
@@ -61,16 +45,16 @@ np.random.seed(36) # set seed value so that the results are reproduceable
 #               (input)--> [Linear->Sigmoid] -> [Linear->Sigmoid]->[Linear->Sigmoid] -->(output)  
 
 #------ LAYER-1 ----- define hidden layer that takes in training data 
-Z1 = toolset.LinearLayer(input_shape=X_train.shape, n_out=30, ini_type='xavier')
+Z1 = toolset.LinearLayer(input_shape=X_train.shape, n_out=60, ini_type='xavier')
 A1 = toolset.SigmoidLayer(Z1.Z.shape)
 
 #------ LAYER-2 ----- define output layer that take is values from hidden layer
-Z2= toolset.LinearLayer(input_shape=A1.A.shape, n_out=30, ini_type='xavier')
+Z2= toolset.LinearLayer(input_shape=A1.A.shape, n_out=60, ini_type='xavier')
 A2= toolset.SigmoidLayer(Z2.Z.shape)
 
 
 #------ LAYER-3 ----- define output layer that take is values from 2nd hidden layer
-Z3= toolset.LinearLayer(input_shape=A2.A.shape, n_out=30, ini_type='xavier')
+Z3= toolset.LinearLayer(input_shape=A2.A.shape, n_out=60, ini_type='xavier')
 A3= toolset.SigmoidLayer(Z3.Z.shape)
 
 # see what random weights and bias were selected and their shape 
