fixed Ground Truth (now is passed as prob distribution vector). fixed Softmax class (now tries to pass bacwards Jacobian) + some minor tweaks

iris_script.py
@@ -32,7 +32,6 @@ dataToTest = dataExpected[130:150, :]
 
 
 
-
 #%%
 
 batch_size = 5
@@ -46,6 +45,11 @@ for i in range(0, len(dataToTrain), batch_size):
 
 X = dataset[0][:,0:4]
 Y = dataset[0][:,4]
+    
+
+#%%
+
+Y = np.array(toolset_new.convert_to_probdist(Y))
 
 X_train = X.T
 Y_train = Y.T
@@ -59,8 +63,7 @@ number_of_epochs = 400
 
 np.random.seed(18) # set seed value so that the results are reproduceable
 
-# Our network architecture has the shape: 
-#               (input)--> [Linear->Sigmoid] -> [Linear->Sigmoid]->[Linear->Sigmoid] -->(output)  
+
 
 #------ LAYER-1 ----- define hidden layer that takes in training data 
 Z1 = toolset_new.LayerLinear(in_features=4, out_features=64)
@@ -74,17 +77,9 @@ A2 = toolset_new.LayerSigmoid()
 Z3 = toolset_new.LayerLinear(in_features=32, out_features=3)
 
 
-SM = toolset_new.LayerSoftmaxV2(in_features=3, out_features=3)
+SM = toolset_new.LayerSoftmaxV2()
 
 #A3 = toolset_new.LayerSigmoid()
-
-
-
-
-# see what random weights and bias were selected and their shape 
-# print(Z1.params)
-# print(Z2.params)
-# print(Z3.params)
 
 
 #%%
@@ -119,8 +114,9 @@ for epoch in range(number_of_epochs):
 
         # ------------------------- forward-prop -------------------------
         
-    
+        
         out = Variable(X)
+        
         for layer in neural_net:
               out = layer.forward(out)
 
@@ -128,31 +124,19 @@ for epoch in range(number_of_epochs):
 
         loss = loss_func.forward(Variable(Y), out)
 
-        #print(f'loss: {loss.value}')
-
         # ------------------------- back-prop ----------------------------
-
-
-        loss_func.backward()
         
-        
-        SM.backward()
+        rev_layers = neural_net[::-1]
 
-        #A3.backward()
-        Z3.backward()
-
-        A2.backward()
-        Z2.backward()
-
-        A1.backward()
-        Z1.backward()
+        for layer in rev_layers:
+              out = layer.backward()
 
         # ----------------------- Update weights and bias ----------------
         
         stuffToUpdate = []
         
         for item in neural_net:
-            if isinstance(item, (toolset_new.LayerLinear,toolset_new.LayerSoftmaxV2)):
+            if isinstance(item, (toolset_new.LayerLinear)):
                 stuffToUpdate.append(item)
             elif isinstance(item, str):
                 pass
@@ -165,19 +149,8 @@ for epoch in range(number_of_epochs):
         print("Cost at epoch#{}: {}".format(epoch, np.mean(loss.value)))
         costs.append(np.mean(loss.value))
         iterationz.append(counter)
-                
-                
-        
-        # See what the final weights and bias are training 
-        # print(Z1.params)
-        # print(Z2.params)
-        # print(Z3.params)
-   
+
 
 #%%
 
 plt.pyplot.scatter(iterationz, costs) # per epoch
-
-#%%
-
-

tools/toolset_new.py
@@ -77,55 +77,64 @@ class MSE_Loss:
         
         
         
-class LayerSoftmax(object):
+# class LayerSoftmax(object):
     
-    def __init__self(self):
-           super().__init__()
-           self.x = None
-           self.output = None
+#     def __init__self(self):
+#            super().__init__()
+#            self.x = None
+#            self.output = None
            
-    def func(self, x):
-        exps = np.exp(x-np.max(x))
-        return exps/np.sum(exps)
+#     def func(self, x):
+#         exps = np.exp(x-np.max(x))
+#         return exps/np.sum(exps)
     
-    def forward(self, x):
-      self.x = x
-      self.output = Variable(
-          self.func(x.value))
-      return self.output
+#     def forward(self, x):
+#       self.x = x
+#       self.output = Variable(
+#           self.func(x.value))
+#       return self.output
         
-    def backward(self):
-        y = self.func(self.x.value)
-        self.x.grad = (y *(1.0 - y)) * self.output.grad
+#     def backward(self):
+#         y = self.func(self.x.value)
+#         self.x.grad = y * (1 - self.output.grad)
         
 
 class LayerSoftmaxV2(object):
     
-    def __init__(self, in_features, out_features):
+    def __init__(self):
         
-        self.w: Variable = Variable(np.random.uniform(low=-1,size=(in_features, out_features)))
-        self.b: Variable = Variable(np.zeros((out_features,)))
-        
+        super().__init__()
         self.x: Variable = None
         self.output: Variable = None
            
     def func(self, x):
-        exps = np.exp(x-np.max(x))
+        exps = np.exp(self.x.value-np.max(self.x.value))
         return exps/np.sum(exps)
     
     def forward(self, x):
       self.x = x
+      self.output = Variable(self.func(self.x.value))
       
-      self.output = Variable(
-          self.func(np.matmul(x.value, self.w.value) + self.b.value))
+      
       return self.output
         
     def backward(self):
-        y = self.func(self.x.value)
+            
+            #m = np.shape(self.y.value)[0]
+            
+        for idx in range(np.shape(self.output.value)[0]):
+            
+            J = np.zeros((np.shape(self.output.value)[1], np.shape(self.output.value)[1]))
+            
+            for i in range(np.shape(self.output.value)[1]):
+                for j in range(np.shape(self.output.value)[1]):
+                    if i == j:
+                        J[i,j] = self.output.value[idx][i] * (1-self.output.value[idx][j])
+                    else:
+                        J[i,j] = -self.output.value[idx][i] * self.output.value[idx][j]
+                    
+        self.x.grad[idx] = np.matmul(J, self.output.grad[idx])
         
-        self.x.grad = y * (1 - self.output.grad)
-    
-    
         
 
 
@@ -136,11 +145,11 @@ class CrossEntropy:
     def __init__(self):
         self.y: Variable = None
         self.y_hat: Variable = None
-        self.gradTop: Variable = None
+        
         
     def forward(self, y: Variable, y_hat: Variable):
         
-        m = np.shape(y.value)[0]
+        #m = np.shape(y.value)[0]
         
         self.y = y
         self.y_hat = y_hat
@@ -153,10 +162,10 @@ class CrossEntropy:
     
     def backward(self):
         
-        m = np.shape(self.y.value)[0]
+        #m = np.shape(self.y.value)[0]
         
         #self.y_hat.grad = (1/m) * ((1/(1+np.exp(- self.y_hat.value))) - self.y.value)
-        self.gradTop = Variable(self.y.value/self.y_hat.value)
+        self.y_hat = self.y.value/self.y_hat.value
         
         
         
@@ -181,6 +190,26 @@ def dataGenByExpression(expr,low_bound,high_bound,length):
     output = np.column_stack((x1_arr, x2_arr,answerz))
     
     return output
+
+def convert_to_probdist(vector):
+    
+    probdists = []
+
+    for row in vector:
+        
+        blank_probdist = np.zeros(3)
+        
+        if row == 0:
+            blank_probdist[0] = 1
+            probdists.append(blank_probdist)
+        elif row == 1:
+            blank_probdist[1] = 1
+            probdists.append(blank_probdist)
+        elif row == 2:
+            blank_probdist[2] = 1
+            probdists.append(blank_probdist)
+    
+    return probdists
 
 
     