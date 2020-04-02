8b480796f3ce76f68798751df9c41b30e35dbeac
Dmitrijs Odinokijs <Dmitrijs.Odinokijs@edu.rtu.lv>
2020-04-02 20:15:48 +0300

    fixed metrics

--- iris_model_loader.py
+++ iris_model_loader.py
@@ -10,6 +10,8 @@ import numpy.core.defchararray as np_f
 import matplotlib as plt
 import pickle
 
+import sklearn
+
 
 with open('datasets/iris.csv', newline='') as csvfile:
      data = list(csv.reader(csvfile))
@@ -57,7 +59,7 @@ for i in range(0, len(dataToTest), batch_size):
 
 #%%
 
-number_of_epochs = 4000
+number_of_epochs = 400
 
 np.random.seed(32) # set seed value so that the results are reproduceable
 
@@ -66,15 +68,17 @@ np.random.seed(32) # set seed value so that the results are reproduceable
 
 costs = []
 accuracies = []
+f1_scores = []
 iterationz = []
 extractionz = []
 counter = 0
 
 loss_func = CrossEntropy()
 
-#oss_func = MSE_Loss()
+#loss_func = MSE_Loss()
 
-instance = MyModel()
+actual_model = MyModel()
+
 
 
 for epoch in range(number_of_epochs):
@@ -82,67 +86,80 @@ for epoch in range(number_of_epochs):
     counter+=1
     
     # np.random.shuffle(dataset)
+    
+    for dataset in [train_dataset, test_dataset]:
       
 
-    for batch in train_dataset:
-
-        np.random.shuffle(batch)
-
-        X_train = batch[:,0:4]
-        Y_train = batch[:,4]
-        Y_train = np.array(toolset_new.convert_to_probdist(Y_train))
-
-        # ------------------------- forward-prop -------------------------    
-        
-        
-        out = instance.forward(X_train)
-        
-        loss = loss_func.forward(Variable(Y_train), out)
-        
-        np.random.shuffle(dataToTest)
-        
-        ground_truth = dataToTest[:,4]
-        ground_truth = np.array(toolset_new.convert_to_probdist(ground_truth))
-        
-        instance_2 = copy.deepcopy(instance)
-        
-        
-
-
-        predict = instance_2.forward(dataToTest[:,0:4])
-        predicted = predict.value
-        
-        correct = 0
-        total = 0
-        for i in range(len(dataToTest)):
-            act_label = np.argmax(ground_truth[i]) # act_label = 1 (index)
-            pred_label = np.argmax(predicted[i]) # pred_label = 1 (index)
-            if(act_label == pred_label):
-                correct += 1
-            total += 1
-        accuracy = (correct/total)
-
-        # argmaxed = predicted.argmax(1)
-        # extracted = np.nonzero(ground_truth - argmaxed)
-        # accuracy = np.size(extracted[0])/np.size(argmaxed)
-        
-
-        loss_func.backward()
-        
-        instance.backward()
-        
+        for batch in train_dataset:
     
-        
-        
-        
-        
+            #np.random.shuffle(batch)
+    
+            X_train = batch[:,0:4]
+            Y_train = batch[:,4]
+            Y_train = np.array(toolset_new.convert_to_probdist(Y_train))
+    
+            # ------------------------- forward-prop -------------------------    
             
+            
+            out = actual_model.forward(X_train)
+            loss = loss_func.forward(Variable(Y_train), out)           
+    
+            loss_func.backward()  
+            actual_model.backward()
+            
+        for batch in test_dataset:
+           
+           #np.random.shuffle(batch)
+           
+           X_test = batch[:,0:4]
+           Y_test = batch[:,4]
+           Y_test = np.array(toolset_new.convert_to_probdist(Y_test))
+           
+           # ------------------------- forward-prop -------------------------    
+           
+           
+           out = actual_model.forward(X_test)
+           loss = loss_func.forward(Variable(Y_test), out)
+
+           
+           #instance_2 = copy.deepcopy(instance)
+           
+           
+          
+           
+           predict = actual_model.forward(X_test)
+           predicted = predict.value
+           
+           
+           
+           correct = 0
+           total = 0
+           true = []
+           pred = []
+           
+           
+           for i in range(len(batch)):
+               act_label = np.argmax(Y_test[i]) # act_label = 1 (index)
+               pred_label = np.argmax(predicted[i]) # pred_label = 1 (index)
+               true.append(act_label)
+               pred.append(pred_label)
+               if(act_label == pred_label):
+                   correct += 1
+               total += 1
+               
+        
+           f1 = sklearn.metrics.f1_score(true, pred, average='macro')
+           accuracy = (correct/total)
+
+                
     if (epoch % 10) == 0:
         print("Cost at epoch#{}: {}".format(epoch, loss.value))
         print("Accuracy --- > {}".format(accuracy))
-        #print("Snapshot --- > {} ---- > {}".format(instance.out.value[0,0],instance_2.out.value[0,0]))
+        print("F1 --- > {}".format(f1))
+       
         costs.append(loss.value)
         accuracies.append(accuracy)
+        f1_scores.append(f1)
         #extractionz.append(extracted)
       
         
@@ -155,34 +172,13 @@ for epoch in range(number_of_epochs):
 
 
 
-
-#%% Testing
-
-# X_test = dataToTest[:,0:4]
-
-# predict = instance.forward(X_test)
-
-# predicted = predict.value
-
-# # #%% Evaluation
-
-
-# argmaxed = predicted.argmax(1)
-# groundTruth = dataToTest[:,4]    
-
-# extracted = np.nonzero(groundTruth - argmaxed)
-
-# accuracy = 1 - np.size(extracted)/np.size(argmaxed)
-
-# print("Accuracy --- > {}".format(accuracy))
-
-
-
-#%%
-
 # Create two subplots and unpack the output array immediately
-f, (ax1, ax2) = plt.pyplot.subplots(2, 1, sharey=False)
+f, (ax1, ax2, ax3) = plt.pyplot.subplots(3, 1, sharey=False)
 ax1.scatter(iterationz, costs)
-ax1.set_title('Loss vs Accuracy')
+ax1.set_title('Loss vs Accuracy vs F1')
 ax2.scatter(iterationz, accuracies)
+ax3.scatter(iterationz, f1_scores)
+
+ax1.set_xticks([])
+ax2.set_xticks([])
 
